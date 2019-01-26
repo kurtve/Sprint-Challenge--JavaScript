a2aeead8cd6b410da958b07a90d42633b70036f4
Kurt Van Etten <kurt@vanetten.us>
2019-01-26 14:41:48 -0500

    Add classes.js stretch goal

--- challenges/classes.js
+++ challenges/classes.js
@@ -29,8 +29,27 @@ console.log(cuboid.surfaceArea()); // 130
 
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.
-//  Find out the formulas for volume and surface area for cubes and create those methods
+// Find out the formulas for volume and surface area for cubes and create those methods
 // using the dimension properties from CuboidMaker.  Test your work by logging out your
 // volume and surface area.
 
+
+// Since Cubemaker only has a single property, we will pass its value directly
+// instead of making it a member of a props object.  The parent's volume and surfaceArea
+// methods will continue to work as is, so no changes needed (that's the whole point
+// of inheritence, right?)
+
+class CubeMaker extends CuboidMaker {
+	constructor(size) {
+		super({length: size, width: size, height: size});
+		this.size = size;
+	}
+}
+
+
+const cubeyMcCubeFace = new CubeMaker(5);
+
+console.log(`I am a cube of size ${cubeyMcCubeFace.size} units.`);
+console.log(`My volume is ${cubeyMcCubeFace.volume()} units cubed.`);
+console.log(`My surface area is ${cubeyMcCubeFace.surfaceArea()} units squared.`);
 
